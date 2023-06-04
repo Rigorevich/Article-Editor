061d1eab512c841ce7937ffb1eeed5c26b1b62ef
Rigorevich Ilya <rigorevich268@gmail.com>
2023-06-04 23:41:17 +0300

    feat: add Header component & routing

---
+++ ui/src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "./Header.module.css";
+import { NavLink, NavLinkProps } from "react-router-dom";
+
+const Header: React.FC = () => {
+  return (
+    <header className={styled.header}>
+      <nav className={styled.navbar}>
+        <ul className={styled.nav_list}>
+          <li className={styled.nav_item}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? styled.active_link : styled.nav_link
+              }
+            >
+              Главная
+            </NavLink>
+          </li>
+          <li className={styled.nav_item}>
+            <NavLink
+              to="/create-article"
+              className={({ isActive }) =>
+                isActive ? styled.active_link : styled.nav_link
+              }
+            >
+              Создать статью
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
