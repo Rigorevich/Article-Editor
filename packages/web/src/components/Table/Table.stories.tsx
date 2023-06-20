2e5039612fbfc2e14187ef377402eb8cbe9d1ef7
Rigorevich Ilya <rigorevich268@gmail.com>
2023-06-20 13:19:04 +0300

    feat: add story for Table component

---
+++ packages/web/src/components/Table/Table.stories.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import Table from "./";
+import { TableNode } from "../../interfaces";
+
+export default {
+  title: "Components/Table",
+  component: Table,
+} as Meta;
+
+const Template: Story<{ table: TableNode }> = (args: { table: TableNode }) => (
+  <Table {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  table: {
+    id: "0",
+    type: "table",
+    data: {
+      tableName: "Example Table",
+      rows: [
+        {
+          id: "1",
+          cols: [
+            { id: "1", value: "Cell 1" },
+            { id: "2", value: "Cell 2" },
+            { id: "3", value: "Cell 3" },
+          ],
+        },
+        {
+          id: "2",
+          cols: [
+            { id: "4", value: "Cell 4" },
+            { id: "5", value: "Cell 5" },
+            { id: "6", value: "Cell 6" },
+          ],
+        },
+      ],
+    },
+  },
+};
