b75e76a1335ca8f962f9679f224ea394b6897862
Rigorevich Ilya <rigorevich268@gmail.com>
2023-07-05 15:37:38 +0300

    fix: change urls

--- packages/web/src/utils/constants/urls.constants.ts
+++ packages/web/src/utils/constants/urls.constants.ts
@@ -1,6 +1,6 @@
 //"https://article-editor-api.vercel.app/trpc"
 //"http://localhost:4000/trpc"
-export const API_URL = 'http://localhost:4000/trpc';
+export const API_URL = 'https://article-editor-api.vercel.app/trpc';
 
 //https://article-editor-hgu607l1y-rigorevich.vercel.app/
-export const APP_URL = 'http://localhost:3000';
+export const APP_URL = 'https://article-editor-hgu607l1y-rigorevich.vercel.app/';
